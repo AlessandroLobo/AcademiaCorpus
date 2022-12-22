7b20cdf2703cf7802cf2dbd024db46ac069f8ca6
Alessandro Lobo <alessandro.lobo@hotmail.com>
2022-12-22 09:31:39 -0300

    ajustes

--- AcademiaCorpus/Controllers/AccountController.cs
+++ AcademiaCorpus/Controllers/AccountController.cs
@@ -9,16 +9,11 @@ namespace AcademiaCorpus.Controllers
         private readonly UserManager<IdentityUser> _userManager;
         private readonly SignInManager<IdentityUser> _signInManager;
 
-
-
         public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
         {
             _userManager = userManager;
             _signInManager = signInManager;
         }
-
-
-
 
         public IActionResult Login(string returnUrl)
         {
@@ -28,32 +23,6 @@ namespace AcademiaCorpus.Controllers
             });
         }
 
-
-        //// O post é obrigatório para o método login
-        //[HttpPost]
-        //public async Task<IActionResult> Login(LoginViewModel loginVM)
-        //{
-        //    if (!ModelState.IsValid)
-        //        return View(loginVM);
-
-        //    var user = await _userManager.FindByNameAsync(loginVM.UserName);
-
-        //    if (user != null)
-        //    {
-        //        var result = await _signInManager.PasswordSignInAsync(user, loginVM.Password, false, false);
-        //        if (result.Succeeded)
-        //        {
-        //            if (string.IsNullOrEmpty(loginVM.ReturnUrl))
-        //            {
-        //                return RedirectToAction("index", "Home");
-        //            }
-        //            return Redirect(loginVM.ReturnUrl);
-        //        }
-        //    }
-        //    ModelState.AddModelError("", "Falha ao realizer o login!!");
-        //    return View(loginVM);
-        //}
-        // O post é obrigatório para o método login
 
         [HttpPost]
         public async Task<IActionResult> Login(LoginViewModel loginVM)
@@ -106,34 +75,11 @@ namespace AcademiaCorpus.Controllers
         }
 
 
-
         public IActionResult Register()
         {
             return View();
         }
 
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public async Task<IActionResult> Register(LoginViewModel registroVM)
-        //{
-        //    if (ModelState.IsValid)
-        //    {
-        //        var user = new IdentityUser { UserName = registroVM.UserName };
-        //        var result = await _userManager.CreateAsync(user, registroVM.Password);
-
-        //        if (result.Succeeded)
-        //        {
-        //            await _signInManager.SignInAsync(user, isPersistent: false);
-        //            await _userManager.AddToRoleAsync(user, "Member");
-        //            return RedirectToAction("Login", "Account");
-        //        }
-        //        else
-        //        {
-        //            this.ModelState.AddModelError("Registro", "Falha ao registrar o usuário");
-        //        }
-        //    }
-        //    return View(registroVM);
-        //}
 
         [HttpPost]
         [ValidateAntiForgeryToken]
@@ -177,15 +123,6 @@ namespace AcademiaCorpus.Controllers
                 return View(registroVM);
             }
         }
-
-        //[HttpPost]
-        //public async Task<IActionResult> Logout()
-        //{
-        //    HttpContext.Session.Clear();
-        //    HttpContext.User = null;
-        //    await _signInManager.SignOutAsync();
-        //    return RedirectToAction("Index", "Home");
-        //}
 
 
         [HttpPost]
